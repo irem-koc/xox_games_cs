926ff462b704430edf409eab0c8acb9d2f3ddf0d
irem-koc <irem_koc_75@hotmail.com>
2023-03-17 21:47:24 +0300

    post request completed

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,22 +1,39 @@
+import axios from "axios";
 import React, { useContext, useEffect } from "react";
 import { Context } from "../../context/Context";
 import "./MainPage.css";
 
 const MainPage = () => {
-    const { day, hour, minute} =
-        useContext(Context);
+    const { day, hour, minute, email, setEmail } = useContext(Context);
     useEffect(() => {}, []);
+    const handleSubmit = async (e) => {
+      e.preventDefault()
+      const url = 'http://78.188.150.116:6566/xoxTask';
+      const data = {
+        "email": email
+      };
+      try {
+        const response = await axios.post(url, data);
+        console.log(response);
+      } catch (error) {
+        console.log(error);
+      }
+    }
     return (
         <div className="middle-section">
             <div className="counter">
                 {day} days {hour} hours {minute} minutes.
             </div>
-            <input
-                className="mail-input"
-                type="text"
-                placeholder="example@example.com"
-            />
-            <button className="notify-btn">Get Notified</button>
+            <form onSubmit={handleSubmit}>
+                <input
+                    onChange={(e) => {setEmail(e.target.value)}}
+                    value={email}
+                    className="mail-input"
+                    type="text"
+                    placeholder="example@example.com"
+                />
+                <button className="notify-btn" type="submit">Get Notified</button>
+            </form>
         </div>
     );
 };
--- src/context/Context.js
+++ src/context/Context.js
@@ -6,7 +6,8 @@ const ContextProvider = ({ children }) => {
     const [day, setDay] = useState(30);
     const [hour, setHour] = useState(23);
     const [minute, setMinute] = useState(59);
-    const values = { day, hour, minute, setDay, setHour, setMinute };
+    const [email, setEmail] = useState()
+    const values = { day, hour, minute, setDay, setHour, setMinute,email, setEmail };
     return <Context.Provider value={values}>{children}</Context.Provider>;
 };
 export default ContextProvider;
